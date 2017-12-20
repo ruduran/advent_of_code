62cfa7dc2b035358656e50bd82a878df11881886
Rubén Durán Balda <ruduran@users.noreply.github.com>
2017-12-20 08:55:56 +0100

    Detect cycles to speed up the process

--- 2017/python/src/aoc/day16/part2.py
+++ 2017/python/src/aoc/day16/part2.py
@@ -8,13 +8,37 @@ from . import BaseProcessor
 
 
 class Processor(BaseProcessor):
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.ocurrences = {}
 
     def process(self):
         self.load_instructions()
-        # TODO: Check positions after the first run and after that just do the exchange
-        for i in tqdm(range(1000000000)):
-            self.run_instructions()
+        self.run_iterations(1000000000)
         return ''.join(self.programs)
+
+    def run_iterations(self, iterations):
+        remaining = 0
+        for i in tqdm(range(iterations)):
+            cycle = self.get_cycle_len(i)
+            if cycle:
+                remaining = iterations - i
+                remaining %= cycle
+                break
+            else:
+                self.run_iteration(i)
+
+        for i in tqdm(range(remaining)):
+            self.run_instructions()
+
+    def run_iteration(self, it):
+        self.ocurrences[''.join(self.programs)] = it
+        self.run_instructions()
+
+    def get_cycle_len(self, current_it):
+        current_status = ''.join(self.programs)
+        if current_status in self.ocurrences:
+            return current_it - self.ocurrences[current_status]
 
 
 def main():
